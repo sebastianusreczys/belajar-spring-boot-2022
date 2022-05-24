4b451d93fcee32e3b6ede18d8ef975bc17a282bc
sebastianusreczys <sebastianusreczy17@gmail.com>
2022-05-24 13:35:32 +0700

    starter project

---
+++ src/test/java/com/reczy/sebastian/BelajarSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reczy.sebastian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
